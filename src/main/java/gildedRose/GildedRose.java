35a2928a249a1eea453b17b160ab2aa49a621077
LUSK <SKYE.LU@oocl.com>
2019-08-12 22:19:19 +0800

    Refactor gildedRose 抽完方法后重构里面代码，对四个方法进行重构

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -2,15 +2,6 @@ package gildedRose;
 
 public class GildedRose {
     Item[] items;
-
-    public Item[] getItems() {
-        return items;
-    }
-
-    public void setItems(Item[] items) {
-        this.items = items;
-    }
-
     public GildedRose(Item[] items) {
         this.items = items;
     }
@@ -18,65 +9,51 @@ public class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-
             if (items[i].name.equals("Aged Brie")) {
                 keepAgedBrie(items[i]);
                 continue;
             }
-            keepOtherMethod(items[i]);
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].quality = items[i].quality - 1;
+            if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                continue;
             }
-            keepBackstage(items[i]);
-
-
+            if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                keepBackstage(items[i]);
+                continue;
+            }
+            keepOtherMethod(items[i]);
         }
     }
 
     private void keepOtherMethod(Item item) {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1;
+        if(item.sellIn < 1){
+            item.quality = Math.max(item.quality-2,0);
+        }else{
+            item.quality = Math.max(item.quality-1,0);
         }
-        item.sellIn = item.sellIn - 1;
-        if (item.quality > 0) {
-            item.quality = item.quality - 1;
-        }
-    }
-
-    private void keepSulfuras(Item item){
-
+        item.sellIn--;
     }
     private void keepBackstage(Item item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-
-            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.sellIn < 11) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-
-                if (item.sellIn < 6) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-            }
+        if(item.sellIn < 1){
+            item.quality = 0;
         }
-        item.sellIn = item.sellIn - 1;
-
-        if (item.sellIn < 0) {
-            item.quality = item.quality - item.quality;
+        if(item.sellIn >= 1 && item.sellIn < 6){
+            item.quality = Math.min(item.quality+3,50);
         }
+        if(item.sellIn >=6 && item.sellIn < 11){
+            item.quality = Math.min(item.quality+1,50);
+        }
+        if(item.sellIn >= 11){
+            item.quality = Math.min(item.quality+1,50);
+        }
+        item.sellIn--;
     }
 
     private void keepAgedBrie(Item item) {
         if (item.getSellIn() < 1) {
             item.quality = Math.min(item.quality + 2, 50);
+        }else{
+            item.quality = Math.min(item.quality + 1, 50);
         }
-        item.quality = Math.min(item.quality + 1, 50);
         item.sellIn--;
     }
 }
--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -11,7 +11,7 @@ public class GildedRoseTest {
         GildedRose gildedRose = new GildedRose(items);
         gildedRose.updateQuality();
         //System.out.println(gildedRose.items[0]);
-        Assert.assertEquals("Aged Brie, 47, 3",gildedRose.items[0].toString());
+        Assert.assertEquals("Aged Brie, 48, 2",gildedRose.items[0].toString());
     }
     @Test
     public void should_return_right_items_when_give_Aged_and_s_and_q_2(){
@@ -19,7 +19,7 @@ public class GildedRoseTest {
         GildedRose gildedRose = new GildedRose(items);
         gildedRose.updateQuality();
         //System.out.println(gildedRose.items[0]);
-        Assert.assertEquals("Aged Brie, 0, 50",gildedRose.items[0].toString());
+        Assert.assertEquals("Aged Brie, 1, 50",gildedRose.items[0].toString());
     }
     @Test
     public void should_return_right_items_when_give_Back_and_s_and_q_1(){
@@ -35,7 +35,7 @@ public class GildedRoseTest {
         GildedRose gildedRose = new GildedRose(items);
         gildedRose.updateQuality();
         //System.out.println(gildedRose.items[0]);
-        Assert.assertEquals("Backstage passes to a TAFKAL80ETC concert, 9, 50",gildedRose.items[0].toString());
+        Assert.assertEquals("Backstage passes to a TAFKAL80ETC concert, 9, 49",gildedRose.items[0].toString());
 
     }
     @Test
@@ -72,6 +72,31 @@ public class GildedRoseTest {
         //System.out.println(gildedRose.items[0]);
         Assert.assertEquals("Sulfuras, Hand of Ragnaros, 12, 44",gildedRose.items[0].toString());
     }
+    @Test
+    public void should_return_right_items_when_give_other_and_s_and_q_1(){
+        Item[] items = new Item[]{new Item("Skye",0,44)};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        //System.out.println(gildedRose.items[0]);
+        Assert.assertEquals("Skye, -1, 42",gildedRose.items[0].toString());
+    }
+    @Test
+    public void should_return_right_items_when_give_other_and_s_and_q_2(){
+        Item[] items = new Item[]{new Item("Skye",2,44)};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        //System.out.println(gildedRose.items[0]);
+        Assert.assertEquals("Skye, 1, 43",gildedRose.items[0].toString());
+    }
+    @Test
+    public void should_return_right_items_when_give_back_and_s_and_q_5(){
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert",0,3)};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        //System.out.println(gildedRose.items[0]);
+        Assert.assertEquals("Backstage passes to a TAFKAL80ETC concert, -1, 0",gildedRose.items[0].toString());
+    }
+
 
 }
 
